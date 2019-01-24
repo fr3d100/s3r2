a20f90c61050248cb8c3ab428f39e7d2ce3e943e
fr3d100 <f.bonnand@gmail.com>
2019-01-24 23:13:20 +0100

    Classes created

--- app.rb
+++ app.rb
@@ -1,2 +1,11 @@
 require 'bundler'
 Bundler.require
+
+#LIAISON AVEC L'APPLICATION
+$:.unshift File.expand_path("../lib/", __FILE__)
+require 'application'
+
+
+# Démarrage du jeu au travers du perfrom du controlleur
+app = Application.new
+app.perform
